3cc70e67b60d42a6d28277356c26f2247e83954d
supto <salatussadik131@gmail.com>
2024-10-17 21:37:42 +0530

    complite

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}","./*.{html,js"],
+  theme: {
+    extend: {
+        colors:{
+            "primary":"#1D183A",
+            "secendary":"#16A8AA",
+            "fontColor":"#33333"
+          }
+    },
+  },
+  plugins: [],
+}
+
